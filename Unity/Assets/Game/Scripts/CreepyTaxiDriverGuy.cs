7f18315710dba2fa1884ea80bbdeff346794b040
robintheilade <robin.theilade@gmail.com>
2013-01-19 19:07:23 -0800

    Added characters

---
+++ Unity/Assets/Game/Scripts/CreepyTaxiDriverGuy.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class CreepyTaxiDriverGuy : MonoBehaviour
+{
+    public Transform player;
+    public float minimumDistance = 100.0f;
+    private float initialPositionX;
+
+    private void Start()
+    {
+        this.initialPositionX = this.transform.position.x;
+    }
+
+    private void Update()
+    {
+        var player = this.player;
+        if (player == null)
+        {
+            return;
+        }
+
+        var distance = player.position.x - this.transform.position.x;
+
+        if (distance > -10 && distance < 10)
+        {
+            return;
+        }
+
+        var position = 0f;
+        // Right
+        if (distance > 0)
+        {
+            position = initialPositionX - Mathf.Lerp(0f, this.minimumDistance, Mathf.Abs(distance) / this.minimumDistance);
+        }
+        else if (distance < 0)
+        {
+            position = initialPositionX + Mathf.Lerp(0f, this.minimumDistance,Mathf.Abs(distance)/this.minimumDistance);
+        }
+        else
+        {
+            position = this.transform.position.x;
+        }
+
+
+        this.transform.position = new Vector3(position, this.transform.position.y, this.transform.position.z);
+        print(this.transform.position);
+    }
+}
