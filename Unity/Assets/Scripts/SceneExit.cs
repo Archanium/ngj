4fcc21981efa536d85da1a270cb8b545e7b434ad
Thomas Faurbye Nielsen <archanium@gmail.com>
2013-01-20 07:24:01 +0100

    Add amazing fading animation on drugs

--- Unity/Assets/Scripts/SceneExit.cs
+++ Unity/Assets/Scripts/SceneExit.cs
@@ -1,10 +1,30 @@
 using UnityEngine;
 
-public class SceneExit : MonoBehaviour
+public class SceneExit : MonoBehaviour, IEventListener
 {
     public bool allowMouseExit = false;
     public bool allowTriggerExit = true;
     public int sceneIndex = 0;
+	public Component player;
+	private EventManager eM;
+	
+	public bool HandleEvent(IEvent e) 
+	{
+		print (bool.TrueString == (e.GetData() as string));
+		if(bool.TrueString == e.GetData() as string) {
+			this.TransformCamera();
+			this.eM.QueueEvent(new FadeInEvent(3));
+			this.eM.DetachListener(this as IEventListener, "FadeEvent");
+		}
+		return true;
+	}
+
+	
+	private void Start()
+	{
+		this.eM = EventManager.instance;
+		
+	}
 
     private void OnTriggerEnter(Collider collider)
     {
@@ -22,14 +42,38 @@ public class SceneExit : MonoBehaviour
 
         this.LoadScene();
     }
-
+	
     private void LoadScene()
     {
-        var transform = Camera.mainCamera.transform;
+		this.eM.AddListener(this as IEventListener, "FadeEvent");
+		this.eM.QueueEvent(new FadeOutEvent(2));
+    }
+	
+	private void TransformCamera() 
+	{
+		var transform = Camera.mainCamera.transform;
         transform.position = new Vector3(
             this.sceneIndex * 1920,
             transform.position.y,
             transform.position.z
             );
-    }
+	}
 }
+	
+public class FadeEvent : IEvent
+	{
+		private bool fadeIn = false;
+		public FadeEvent(bool fadeIn)
+		{
+			this.fadeIn = fadeIn;
+		}
+		string IEvent.GetName()
+	    {
+	        return this.GetType().ToString();
+	    }
+ 
+	    object IEvent.GetData()
+	    {
+	        return this.fadeIn.ToString();
+	    }		
+	}
